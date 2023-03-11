89dc9afb707f957ea5e7678deafcccd740d932ff
Марина Никонорова <89656135678@mail.ru>
2023-03-11 10:34:59 +0300

    Добавьте файлы проекта.

---
+++ ChatExam/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using ChatExam.DataBase;
+using ChatExam.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatExam
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ChatTwoEntities db = new ChatTwoEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBLogin.Text == String.Empty || TBPassword.Text == String.Empty)
+            {
+                MessageBox.Show("Введите данные!");
+                return;
+            }
+
+            var auth = db.Employee.FirstOrDefault(x => x.UserName == TBLogin.Text && x.Password == TBPassword.Text);
+
+            if (auth != null)
+            {
+                new GeneralWindow().Show();
+                this.Close();
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Такого пользователя не существует!");
+            }
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ ChatExam/Windows/GeneralWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ChatExam.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChatExam.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для GeneralWindow.xaml
+    /// </summary>
+    public partial class GeneralWindow : Window
+    {
+        public ChatTwoEntities db = new ChatTwoEntities();
+        public GeneralWindow()
+        {
+            InitializeComponent();
+            LVGChat.ItemsSource = db.ChatMessage.ToList();
+            
+        }
+
+        private void FinderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new EmpFinderWindow().Show();
+            
+        }
+
+        private void CloseApplicBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
